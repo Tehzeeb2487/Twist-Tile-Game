3c0ba88f0b093ea45c8fc52fc98e99578164281a
Tehzeeb2487 <tehzeebjahan820@gmail.com>
2025-05-23 23:03:38 +0530

    update Logo

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom"; 
 import './css/Welcome.css';
-
-const Logo = "./logo.png";
+import Logo from '../Logo.png'
 
 const Welcome = () => {
   const navigator = useNavigate();
