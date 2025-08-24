331d81055d93639f75d718bbfd7c73ae635d81b5
Tehzeeb2487 <tehzeebjahan820@gmail.com>
2025-08-25 01:09:34 +0530

    adding something new

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import LevelSelection from "./components/LevelSelection";
 import LevelOne from "./components/pages/LevelOne";
